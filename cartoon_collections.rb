4e29080812efaa6ee6c2e224f37c5563239dbdb7
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-22 14:09:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,5 @@
-def roll_call_dwarves
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each do |dwarf_order| puts "#{dwarf_order.capitalize}"
 end
 
 def summon_captain_planet# code an argument here
