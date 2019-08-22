a0162b0e12d79bd9d2f74c30978494b9121b29ad
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-22 15:13:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,5 +28,5 @@ def find_the_cheese(string_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   count = 0
-  return string_cheese
+  return string_cheese[1]
 end
